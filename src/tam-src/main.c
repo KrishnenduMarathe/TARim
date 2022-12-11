2e6a2c0e56c68f127b072474b777f99e57744dd7
Krishnendu Marathe <kd.workonly.47@gmail.com>
2022-12-11 21:46:28 +0530

    tam initiated

--- src/tam-src/main.c
+++ src/tam-src/main.c
@@ -24,62 +24,382 @@
 **    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "../libtarim.h"
-
-const void ascii_art()
-{
-	const char ascii_art[] = "\n \
-     #######################################################################\n \
-   ##                                                                       ##\n \
- ##     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\        \\\\\\\\                 \\\\\\\\      \\\\\\\\        ##\n \
- ##     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\       \\\\\\\\\\\\               \\\\\\\\\\\\    \\\\\\\\\\\\       ##\n \
- ##             |||             \\\\\\    \\\\\\           |||    ||  ||    |||     ##\n \
- ##             |||           |||        |||         |||    ||  ||    |||     ##\n \
- ##             |||           |||        |||         |||    ||  ||    |||     ##\n \
- ##              |             ||________||          |||    ||  ||    |||     ##\n \
- ##              |             ||________||          |||     ____     |||     ##\n \
- ##             |||           |||        |||         |||              |||     ##\n \
- ##             |||           |||        |||         |||              |||     ##\n \
- ##             |||           |||        |||         |||              |||     ##\n \
- ##            |||||    <^>  |||||      |||||  <^>  |||||            |||||    ##\n \
- ##                                                                           ##\n \
- ##                                                  TARIM ARCHIVE MANAGER    ##\n \
- ##                                                  ---------------------    ##\n \
- ##                                                                           ##\n \
- ##                                                                           ##\n \
- ##      ============================================                         ##\n \
- ##     | @Author:      Krishnendu Marathe           |                        ##\n \
- ##     | @Repository:  github.com/KrishnenduMarathe |                        ##\n \
- ##     | @Association: youtube.com/@kodespace       |                        ##\n \
- ##      ============================================                         ##\n \
- ##                                                                           ##\n \
- ##                                                                           ##\n \
- ##                                          +---------------------------+    ##\n \
- ##                                          |          FEATURE          |    ##\n \
- ##                                          +===========================+    ##\n \
- ##                                          |           Just Like 'TAR' |    ##\n \
- ##                                          |  New file format (.tarim) |    ##\n \
- ##                                          | Selective File Extraction |    ##\n \
- ##                                          |     Custom File Structure |    ##\n \
- ##                                          | Uses Tarim shared Library |    ##\n \
- ##                                          |       Supports Encryption |    ##\n \
- ##                                          |          Requires OpenSSL |    ##\n \
- ##                                          +---------------------------+    ##\n \
-   ##                                                                       ##\n \
-     #######################################################################\n \
-\n\n";
-
-	printf("%s\n", ascii_art);
-}
-
+#include "tam.h"
 
 int main(int argc, char** argv)
 {
-	if (argc < 3)
+	// Atleast 2 Arguments Accepted
+	if (argc < 2)
 	{
 		printf("\nInvalid Number of Arguments\n\t(Use 'tam.x86/tam.x64 -h' for more information.)\n");
 		return 1;
 	}
-	ascii_art();
+
+	// Help Text
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+	{
+		help_text();
+		return 0;
+	}
+
+	// Verify Arguments
+	int create_flag = 0;
+	int extract_flag = 0;
+	int encrypt_flag = 0;
+	int out_flag = 0;
+	int view_flag = 0;
+
+	int option_num = 0;
+	int create_init = 0;
+	int create_count = 0;
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--create") == 0)
+		{
+			create_flag = 1;
+			create_init = 1;
+			if (extract_flag)
+			{
+				printf("\nCannot Create and Extract Archive together\n");
+				return 0;
+			}
+			if (view_flag)
+			{
+				printf("\nCannot Create and View Archive together\n");
+				return 0;
+			}
+			option_num++;
+		}
+		else if (strcmp(argv[i], "-x") == 0 || strcmp(argv[i], "--extract") == 0)
+		{
+			extract_flag = 1;
+			create_init = 0;
+			if (create_flag)
+			{
+				printf("\nCannot Extract and Create Archive together\n");
+				return 0;
+			}
+			if (view_flag)
+			{
+				printf("\nCannot Extract and View Archive together\n");
+				return 0;
+			}
+			if (encrypt_flag)
+			{
+				printf("\nCannot Extract and operate Encryption together\n");
+				return 0;
+			}
+			option_num++;
+		}
+		else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--view") == 0)
+		{
+			view_flag = 1;
+			create_init = 0;
+			if (create_flag)
+			{
+				printf("\nCannot View and Create Archive together\n");
+				return 0;
+			}
+			if (extract_flag)
+			{
+				printf("\nCannot View and Extract Archive together\n");
+				return 0;
+			}
+			if (encrypt_flag)
+			{
+				printf("\nCannot View and operate Encryption together\n");
+				return 0;
+			}
+			if (out_flag)
+			{
+				printf("\nCannot Vew and Specify Output together\n");
+				return 0;
+			}
+			option_num++;
+		}
+		else
+		{
+			if (strcmp(argv[i], "-E") == 0 || strcmp(argv[i], "--encrypt") == 0)
+			{
+				encrypt_flag = 1;
+				create_init = 0;
+				if (view_flag)
+				{
+					printf("\nCannot Encrypt and View Archive together\n");
+					return 0;
+				}
+				if (extract_flag)
+				{
+					printf("\nNo need to pass Encrption Algorithm when Extracting\n");
+					return 0;
+				}
+			}
+			else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--out") == 0)
+			{
+				out_flag = 1;
+				create_init = 0;
+				if (view_flag)
+				{
+					printf("\nCannot Specify Output and View Archive together\n");
+					return 0;
+				}
+			}
+			else
+			{
+				if (create_flag && create_init)
+				{
+					create_count++;
+				}
+			}
+		}
+	}
+
+	// Exit if no valid options
+	if (option_num == 0)
+	{
+		printf("\nInvalid Command Arguments\n");
+		return 0;
+	}
+	if (create_flag && create_count <= 0)
+	{
+		printf("\nCreate Operaton should have atleast one Argument\n");
+		return 0;
+	}
+	
+	// Read Argments
+	int read_args = 0;
+	int read_itr = 0;
+	if (create_count == 0)
+	{ create_count = 1; }
+
+	char readArguments[create_count][FP_MAX];
+	char extractArgument[FP_MAX];
+	TARIM_CRYPT_MODES encryptionMode = NO_ENCRYPT;
+	char outArgument[FP_MAX];
+	char viewArgument[FP_MAX];
+
+	for (int i = 1; i < argc; i++)
+	{
+		// Read Arguments
+		if (read_args == 1)
+		{
+			strcpy(readArguments[read_itr], argv[i]);
+			read_itr++;
+
+			if (read_itr >= create_count)
+			{
+				read_args = 0;
+			}
+		}
+		else if (read_args == 2)
+		{
+			read_args = 0;
+			continue;
+		}
+
+		if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--create") == 0)
+		{
+			read_args = 1;
+		}
+		if (strcmp(argv[i], "-x") == 0 || strcmp(argv[i], "--extract") == 0)
+		{
+			read_args = 2;
+			if (i+1 >= argc)
+			{
+				printf("\nExtract Operaton has no Argument\n");
+				return 1;
+			}
+			strcpy(extractArgument, argv[i+1]);
+		}
+		if (strcmp(argv[i], "-E") == 0 || strcmp(argv[i], "--encrypt") == 0)
+		{
+			//Algorithms: aes256, aria256 & camellia256
+			read_args = 2;
+			if (i+1 >= argc)
+			{
+				printf("\nAlgorithm not specified\n");
+				return 1;
+			}
+			
+			if (strcmp(argv[i+1], "aes256") == 0)
+			{
+				encryptionMode = AES_256_CBC;
+			}
+			else if (strcmp(argv[i+1], "aria256") == 0)
+			{
+				encryptionMode = ARIA_256_CBC;
+			}
+			else if (strcmp(argv[i+1], "camellia256") == 0)
+			{
+				encryptionMode = CAMELLIA_256_CBC;
+			}
+			else
+			{
+				printf("\nUnknown Algorithm %s passed\n", argv[i+1]);
+				return 1;
+			}
+		}
+		if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--out") == 0)
+		{
+			read_args = 2;
+			if (i+1 >= argc)
+			{
+				printf("\nOut Operaton has no Argument\n");
+				return 1;
+			}
+			strcpy(outArgument, argv[i+1]);
+		}
+		if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--view") == 0)
+		{
+			read_args = 2;
+			if (i+1 >= argc)
+			{
+				printf("\nView Operaton has no Argument\n");
+				return 1;
+			}
+			strcpy(viewArgument, argv[i+1]);
+		}
+	}
+
+	// DEBUG
+	printf("Create Options: \n");
+	for (int j = 0; j < create_count; j++)
+	{
+		printf("\t%s\n", readArguments[j]);
+	}
+	printf("\nExtract Options: %s\n", extractArgument);
+	printf("\nEncrypt Options: %d\n", encryptionMode);
+	printf("\nOut Options: %s\n", outArgument);
+	printf("\nView Options: %s\n\n", viewArgument);
+
+	// Required Variables
+	unsigned char* key = NULL;
+	TARIM_METADATA meta;
+	TARIM_FILESAVE* fsave = NULL;
+
+	// View Operation
+	if ( extract_flag || view_flag)
+	{
+		FILE* archive = fopen(viewArgument, "rb");
+		if (archive == NULL)
+		{
+			printf("\nFailed to open Archive %s\n", viewArgument);
+			return 1;
+		}
+		fsave = read_metadata_filedb(&meta, archive);
+
+		display_tree(meta, fsave);
+
+		if (extract_flag)
+		{
+			int all_flag = 0;
+
+			printf("\n[-1]  All Files\nChoose Files to Extract. Multiple files can be specifies in comma separated fashion.\n.Files to Extract: ");
+			char files[8096];
+			scanf("%s", files);
+
+			int o_count = 0;
+			for (int i = 0; i < strlen(files); i++)
+			{
+				if (files[i] == ',')
+				{ o_count++; }
+			}
+			
+			int clear = 1;
+			int iteration = 0;
+			int file_no[o_count];
+			char buffer[1024];
+			for (int i = 0; i < strlen(files); i++)
+			{
+				if (clear)
+				{
+					strcpy(buffer, "");
+					clear = 0;
+				}
+				if (files[i] == ',')
+				{
+					if (i > 0)
+					{
+						// check if buffer contains number
+						file_no[iteration] = atoi(buffer);
+						iteration++;
+						clear = 1;
+					}
+				}
+				else
+				{
+					strncat(buffer, &files[i], 1);
+				}
+
+			}
+
+			// Check for all marker
+			for (int i = 0; i < o_count; i++)
+			{
+				if (file_no[i] == -1)
+				{
+					all_flag = 1;
+					break;
+				}
+			}
+
+			// Check for Encryption and get Key
+			if (meta.encrypt != NO_ENCRYPT)
+			{
+				key = gen_256_key(NULL);
+			}
+
+			int bar_length = 25;
+			if (all_flag)
+			{	
+				for (long long int itr = 0; itr < meta.numFile+meta.numFolder; itr++)
+				{
+					if (fsave[itr].type != FS_FILE)
+					{ continue; }
+
+					// Extract File
+					if (extract_file(meta, fsave, archive, key, itr))
+					{
+						if (meta.encrypt != NO_ENCRYPT)
+						{ free(key); }
+						fclose(archive);
+						free(fsave);
+						return 1;
+					}
+
+					long long int percent_done = (itr+1)/(meta.numFile+meta.numFolder)*100;
+					update_progress_bar(percent_done);
+				}	
+			}
+			else
+			{
+				for (int i = 0; i < o_count; i++)
+				{
+					if (file_no[i] <= 0)
+					{ continue; }
+
+					// Extract File
+					if (extract_file(meta, fsave, archive, key, file_no[i]))
+					{
+						if (meta.encrypt != NO_ENCRYPT)
+						{ free(key); }
+						fclose(archive);
+						free(fsave);
+						return 1;
+					}
+
+					long long int percent_done = (i+1)/o_count*100;
+					update_progress_bar(percent_done);
+				} // MODIFY readwrite.c to accept base address
+			}
+			printf("\r");
+		}
+		
+		// Clean up
+		if (meta.encrypt != NO_ENCRYPT)
+		{ free(key); }
+		fclose(archive);
+		free(fsave);
+	}	
 	return 0;
 }
---
+++ src/tam-src/tam.c
@@ -0,0 +1,131 @@
+// AUTHOR: Krishnendu Marathe
+// PROGRAM: TARIM Archve Manager (TAM)
+
+/*
+** 					 TARIM Archive Manager (TAM)
+** 					 ---------------------------
+** 	  Archive Manager for custom tape archive format with added features
+**    ".tarim" (for  tar improved).
+**
+** 					 	  	 License
+** 	  						 -------
+**
+** 	  This program is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "tam.h"
+
+void ascii_art()
+{
+	const char ascii_art[] = "\n \
+     #######################################################################\n \
+   ##                                                                       ##\n \
+ ##     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\        \\\\\\\\                 \\\\\\\\      \\\\\\\\        ##\n \
+ ##     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\       \\\\\\\\\\\\               \\\\\\\\\\\\    \\\\\\\\\\\\       ##\n \
+ ##             |||             \\\\\\    \\\\\\           |||    ||  ||    |||     ##\n \
+ ##             |||           |||        |||         |||    ||  ||    |||     ##\n \
+ ##             |||           |||        |||         |||    ||  ||    |||     ##\n \
+ ##              |             ||________||          |||    ||  ||    |||     ##\n \
+ ##              |             ||________||          |||     ____     |||     ##\n \
+ ##             |||           |||        |||         |||              |||     ##\n \
+ ##             |||           |||        |||         |||              |||     ##\n \
+ ##             |||           |||        |||         |||              |||     ##\n \
+ ##            |||||    <^>  |||||      |||||  <^>  |||||            |||||    ##\n \
+ ##                                                                           ##\n \
+ ##                                                  TARIM ARCHIVE MANAGER    ##\n \
+ ##                                                  ---------------------    ##\n \
+ ##                                                                           ##\n \
+ ##                                                                           ##\n \
+ ##      ============================================                         ##\n \
+ ##     | @Author:      Krishnendu Marathe           |                        ##\n \
+ ##     | @Repository:  github.com/KrishnenduMarathe |                        ##\n \
+ ##     | @Association: youtube.com/@kodespace       |                        ##\n \
+ ##      ============================================                         ##\n \
+ ##                                                                           ##\n \
+ ##                                                                           ##\n \
+ ##                                          +---------------------------+    ##\n \
+ ##                                          |          FEATURE          |    ##\n \
+ ##                                          +===========================+    ##\n \
+ ##                                          |           Just Like 'TAR' |    ##\n \
+ ##                                          |  New file format (.tarim) |    ##\n \
+ ##                                          | Selective File Extraction |    ##\n \
+ ##                                          |     Custom File Structure |    ##\n \
+ ##                                          | Uses Tarim shared Library |    ##\n \
+ ##                                          |       Supports Encryption |    ##\n \
+ ##                                          |          Requires OpenSSL |    ##\n \
+ ##                                          +---------------------------+    ##\n \
+   ##                                                                       ##\n \
+     #######################################################################\n \
+\n";
+
+	printf("%s\n", ascii_art);
+}
+
+void help_text()
+{
+	const char help_text[] = "\n \
+Usage: tam [options] [Arguments] ...\n \
+Create/Extract/View '.tarim' archives from command line.\n\n \
+ [options]      [Arguments]              [Description]\n \
+ ---------      -----------              -------------\n\n \
+ -h, --help      <none>                   Display help text\n\n \
+ -c, --create    File/Directory Path(s)   Create Archive\n \
+                                          (File Paths are maintained in Archive and Extraction)\n\n \
+ -x, --extract   Archive Path             Extract the archive given\n\n \
+ -E, --encrypt   Algorithm                (optional) Enable Encryption.\n \
+                                          (Algorithms: aes256, aria256 & camellia256)\n\n \
+ -o, --out       Output File/Directory    (optional) Defaults to Current Directory for extraction\n \
+                                          and creates new archive tam.tarim in current director.\n \
+                                          Path & Filename of the Archive created\n \
+                                          Path to Directory to Extract into\n\n \
+ -v, --view      Path to the Archive      Display the content of the archive\n \
+\n";
+
+	printf(help_text);
+}
+
+void display_tree(const TARIM_METADATA meta, const TARIM_FILESAVE* fsave)
+{
+	long long int max = meta.numFile + meta.numFolder;
+	for (long long itr = 0; itr < max; itr++)
+	{
+		if (fsave[itr].type != FS_FOLDER)
+		{
+			printf("[%llu]  %s (%lu)\n", itr+1, fsave[itr].fpath, fsave[itr].fsize);
+		}
+	}
+}
+
+void update_progress_bar(long long int percent_done)
+{
+	const int bar_length = 40;
+
+	int num_char = percent_done * bar_length / 100;
+	printf("\t||");
+	for (int e = 0; e < bar_length; e++)
+	{
+		if (e < num_char)
+		{
+			printf("#");
+		}
+		else
+		{
+			printf(">");
+		}
+	}
+
+	printf("||\t%d%%", percent_done);
+	fflush(stdout);
+}
+
